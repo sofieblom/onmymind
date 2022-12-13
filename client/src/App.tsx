1b6d80b7c308003c200577cadc958932a68877da
sofieblom <ssofieblom@hotmail.com>
2022-12-13 16:17:30 +0100

    set up express server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [testing, setTesting] = useState("");
+  const test = async () => {
+    axios.get("http://localhost:5000/api").then((response) => {
+      setTesting(response.data);
+    });
+  };
+
+  console.log("testing", testing);
+
+  useEffect(() => {
+    test();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
