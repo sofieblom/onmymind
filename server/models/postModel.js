f7c22db2adc8de0a10a8853df0e6939549866b1e
sofieblom <ssofieblom@hotmail.com>
2022-12-21 12:00:35 +0100

    adds a post schema

---
+++ server/models/postModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const PostSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        ref: 'User'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        type: String, 
+        require: true
+    }, 
+    description: String, 
+    require: true
+})
+
+const User = mongoose.model("Post", PostSchema)
+module.exports = User;
