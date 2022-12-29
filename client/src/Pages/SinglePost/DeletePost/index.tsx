9d97839527b1a7948bf9d6fff95993652df01e6a
sofieblom <ssofieblom@hotmail.com>
2022-12-29 11:56:44 +0100

    can delete post

--- client/src/Pages/SinglePost/DeletePost/index.tsx
+++ client/src/Pages/SinglePost/DeletePost/index.tsx
@@ -1,3 +1,23 @@
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
 export const DeletePost = () => {
-  return <button>Delete</button>;
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem("token");
+
+  const handleDelete = () => {
+    console.log("delete");
+    axios
+      .delete(`http://localhost:5000/posts/delete/${params.id}`, {
+        headers: { "x-api-token": token },
+      })
+      .then((response) => {
+        console.log("delete response", response);
+        navigate("/home");
+      });
+  };
+
+  return <button onClick={handleDelete}>Delete</button>;
 };
