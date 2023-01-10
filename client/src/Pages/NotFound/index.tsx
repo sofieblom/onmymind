2303227a5219aa2e94994870193446eaac4daba1
sofieblom <ssofieblom@hotmail.com>
2023-01-10 14:29:33 +0100

    not found link

--- client/src/Pages/NotFound/index.tsx
+++ client/src/Pages/NotFound/index.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import styles from "./styles.module.scss";
 
 export const NotFound = () => {
+  const token = localStorage.getItem("token");
+
   return (
-    <div>
-      <h1>NOT FOUND</h1>
-      <Link to="/">Go home</Link>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>OOOPS, PAGE NOT FOUND</h1>
+
+      {token ? <Link to="/home">Go home</Link> : <Link to="/">Go home</Link>}
     </div>
   );
 };
